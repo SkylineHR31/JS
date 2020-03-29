5a342e99c55442c9333a359f703f8eb72a0ff67c
Aleksandr <Skyline_hr_33@outlook.com>
2020-03-30 01:14:32 +0300

    added foreach, map, filter functions

--- scripts/eighth.js
+++ scripts/eighth.js
@@ -27,3 +27,34 @@ function getSymbolIndex(str, symbol) {
 console.log(getSymbolIndex("hello lol","h"));
 console.log(getSymbolIndex("hello lol","l"));
 console.log(getSymbolIndex("hello lol","v"));
+
+
+const arr = [1, 2, 3, 4, 5,];
+
+function forEach(array, callback) {
+    for (let i = 0; i < array.length; i++) callback(array[i], i);
+}
+
+forEach(arr, arrayOutput => console.log(arrayOutput));
+
+
+function map(array, callback) {
+    let bufferArray = [];
+    for (let i = 0; i < array.length; i++) bufferArray.push(callback(array[i], i));
+    return bufferArray;
+}
+
+let resultArray = map(arr, arrayOutput => arrayOutput);
+console.log(resultArray);
+
+
+function filter(array, callback) {
+    let bufferArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callback) bufferArray.push(array[i]);
+    }
+
+    return bufferArray;
+}
+
+// filter(arr, arrayFilter => func() {});
