b16007307c87fc726bde701223f243ba546e3679
Aleksandr <Skyline_hr_33@outlook.com>
2020-04-03 18:35:54 +0300

    fixed bug in copy function

--- scripts/ninth-lesson.js
+++ scripts/ninth-lesson.js
@@ -13,30 +13,38 @@ const origin = {
     },
     fieldFour: "someValueFour",
     fieldFive: [1, 2, 3, 4, 5,],
-    fieldSix: [{
-        subFieldOneTest: "subSomeValueOne",
-        subFieldTwoTest: "subSomeValueTwo",
-    }, {
-        subFieldOneTest: "subSomeValueOne",
-        subFieldTwoTest: "subSomeValueTwo",
-    }, {
-        subFieldOneTest: "subSomeValueOne",
-        subFieldTwoTest: "subSomeValueTwo",
-    },],
+    fieldSix: [
+        {
+            subFieldOneTest: "subSomeValueOne",
+            subFieldTwoTest: "subSomeValueTwo",
+        },
+        {
+            subFieldOneTest: "subSomeValueOne",
+            subFieldTwoTest: "subSomeValueTwo",
+        },
+        {
+            subFieldOneTest: "subSomeValueOne",
+            subFieldTwoTest: "subSomeValueTwo",
+        },
+    ],
 };
 
 const target = {};
 
 function isObject(obj) {
-    return obj !== null && typeof obj === "object" && obj!== Array.isArray(obj);
+    return obj !== null && typeof obj === "object" && !Array.isArray(obj);
 }
 
 
 function copy(target, origin) {
     for (const key in origin) {
         if (Array.isArray(origin[key])) {
-            target[key] = copy([], origin[key]);
-        } else if(isObject(origin[key])) {
+            let bufferArray = [];
+            for (const iterator of origin[key]) {
+                copy(bufferArray, origin[key]);
+            }
+            target[key] = bufferArray;
+        } else if (isObject(origin[key])) {
             target[key] = copy({}, origin[key]);
         } else {
             target[key] = origin[key];
@@ -49,7 +57,7 @@ copy(target, origin);
 
 console.log(target);
 
-if (origin.fieldSix.subFieldOneTest === target.fieldSix.subFieldOneTest) {
+if (origin.fieldSix[1] === target.fieldSix[1]) {
     console.log("origin.fieldSix.subFieldOneTest EQUAL target.fieldSix.subFieldOneTest");
 } else {
     console.log("origin.fieldSix.subFieldOneTest NOT_EQUAL target.fieldSix.subFieldOneTest");
