43054186aed5daa1a951915eb876827938bec540
Aleksandr <Skyline_hr_33@outlook.com>
2020-04-17 00:43:37 +0300

    rewrite data-print function in custom realization

--- scripts/twelfth-lesson.js
+++ scripts/twelfth-lesson.js
@@ -1,6 +1,19 @@
 function dataPrint() {
     const date = new Date();
-    console.log(date.toLocaleTimeString());
+    let hoursbuffer = String(date.getHours());
+    let minutesbuffer = String(date.getMinutes());
+    let secondsbuffer = String(date.getSeconds());
+
+    function formateData(buffer, outputString) {
+        if (buffer.length < 2) {
+            outputString = "0" + buffer;
+        }
+        
+        return buffer; 
+    }
+    
+    console.log(`${formateData(hoursbuffer, "")}:${formateData(minutesbuffer, "")}:${formateData(secondsbuffer, "")}`);
+    
 }
 
 let timerId = setInterval(() => {
