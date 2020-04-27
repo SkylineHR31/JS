200882d5e8aa122f08dd9855a9cc7cedea566bfa
Aleksandr <Skyline_hr_33@outlook.com>
2020-04-27 16:48:12 +0300

    added list event

--- DOM-lessons/scripts/events.js
+++ DOM-lessons/scripts/events.js
@@ -1,45 +1,70 @@
-const buttonPlus = document.querySelectorAll(".btn-plus");
-const buttonMinus = document.querySelectorAll(".btn-minus");
-const counters = document.querySelectorAll(".counter");
-const buttons = document.querySelectorAll(".square");
+function setSquareCounterListener() {
+    const buttonPlus = document.querySelectorAll(".btn-plus");
+    const buttonMinus = document.querySelectorAll(".btn-minus");
+    const counters = document.querySelectorAll(".counter");
+    const buttons = document.querySelectorAll(".square");
 
-for (let i = 0; i < buttons.length; i++) {
-    let button = buttons[i];
-    let counter = counters[i];
+    for (let i = 0; i < buttons.length; i++) {
+        let button = buttons[i];
+        let counter = counters[i];
 
-    button.addEventListener("click", event => {
-        if (event.target === buttonPlus[i]) {
-            counter.value++;
-        }
+        button.addEventListener("click", event => {
+            if (event.target === buttonPlus[i]) {
+                counter.value++;
+            }
+
+            if (event.target === buttonMinus[i]) {
+                counter.value--;
+
+            }
+
+            if (counter.value < 0) counter.value = 0;
+        });
+    }
+}
+
+function setSquareListener() {
+
+    const squares = document.querySelectorAll(".change-color");
+
+    for (let i = 0; i < squares.length; i++) {
+        let square = squares[i];
+
+        square.addEventListener("click", () => {
+            if (square.classList.contains("default-color")) {
+                square.classList.remove("default-color");
+                square.classList.add("lightsalmon-color");
+            } else if (square.classList.contains("lightsalmon-color")) {
+                square.classList.remove("lightsalmon-color");
+                square.classList.add("lightgreen-color");
+            } else if (square.classList.contains("lightgreen-color")) {
+                square.classList.remove("lightgreen-color");
+                square.classList.add("lightgoldenrodyellow-color");
+            } else if (square.classList.contains("lightgoldenrodyellow-color")) {
+                square.classList.remove("lightgoldenrodyellow-color");
+                square.classList.add("default-color");
+            }
+        });
+    }
+}
+
+function setListListener() {
+    const list = document.querySelector(".list");
+    const items = document.querySelectorAll(".list-item");
     
-        if (event.target === buttonMinus[i]) {
-            counter.value--;
-            
-        }
+    list.addEventListener("click", event => {
+        items.forEach(element => {
+            if (element.classList.contains("red-bcg")) {
+                element.classList.remove("red-bcg");
+            }
 
-        if (counter.value < 0) counter.value = 0;
+            event.target.classList.add("red-bcg");
+        });
     });
 }
 
-const squares = document.querySelectorAll(".change-color");
+setListListener();
 
-for (let i = 0; i < squares.length; i++) {
-    let square = squares[i];
+setSquareCounterListener();
 
-    square.addEventListener("click", () => {
-        if(square.classList.contains("default-color")) {
-            square.classList.remove("default-color");
-            square.classList.add("lightsalmon-color");
-        }
-
-        if(square.classList.contains("lightsalmon-color")) {
-            square.classList.remove("lightsalmon-color");
-            square.classList.add("lightgreen-color");
-        }
-
-        if(square.classList.contains("lightgreen-color")) {
-            square.classList.remove("lightgreen-color");
-            square.classList.add("lightgoldenrodyellow-color");
-        }
-    });
-}
+setSquareListener();
