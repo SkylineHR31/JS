5e3edf574effbc731b78c538a67434a68d1892a1
Aleksandr <Skyline_hr_33@outlook.com>
2020-06-08 15:14:26 +0300

    added xhr homework

---
+++ DOM-lessons/scripts/xml-http-request.js
@@ -0,0 +1,82 @@
+const button = document.querySelector("button");
+let xhrResponse;
+
+function getData(url, onSuccess, onError, onStart, onEnd) {
+    const xhr = new XMLHttpRequest();
+
+    if (xhrResponse) {
+        xhr.open("GET", xhrResponse.info.next);
+    } else {
+        xhr.open("GET", url);
+    }
+
+    xhr.responseType = "json";
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            onSuccess(xhr.response);
+            xhrResponse = xhr.response;
+        } else {
+            onError(xhr.response);
+        }
+        onEnd();
+    }
+
+    xhr.onerror = function () {
+        onError("Error sending request");
+        onEnd();
+    }
+
+    xhr.onabort = function () {
+        console.log("ABORTED!!!");
+        onEnd();
+    }
+
+    onStart();
+
+    xhr.send();
+}
+
+function createUserList(characters) {
+    const list = document.createElement("ul");
+
+    for (const character of characters) {
+        const listItem = document.createElement("li");
+        const textElement = document.createTextNode(character.name);
+
+        listItem.append(textElement);
+        list.append(listItem);
+    }
+
+    return list;
+}
+
+function showList(list) {
+    const container = document.querySelector("#results");
+    if (container !== null) {
+        container.append(list);
+    }
+}
+
+button.addEventListener("click", (event) => {
+    function onSuccess(data) {
+        const list = createUserList(data.results);
+        showList(list);
+    }
+
+    function onError(err) {
+        console.error(err);
+    }
+
+    function onStart() {
+        event.target.innerText = "GETTING...";
+        event.target.disabled = true;
+    }
+
+    function onEnd() {
+        event.target.innerText = "GET GATA";
+        event.target.disabled = false;
+    }
+
+    getData("https://rickandmortyapi.com/api/character", onSuccess, onError, onStart, onEnd);
+});
